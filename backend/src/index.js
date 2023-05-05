ca6518ef5b01201c467d1177a9221ee98b593e01
CHAEWONSEOK98 <7dugod7@naver.com>
2023-05-05 16:52:11 +0900

    back initState

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../uploads')));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('연결 완료');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.get('/', (req, res, next) => {
+  setImmediate(() => {
+    next(new Error('it is an error'));
+  });
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.send(error.message || '서버 에러');
+});
+
+app.listen(4000, () => {
+  console.log(`${port}번에서 실행이 되었습니다`);
+});
